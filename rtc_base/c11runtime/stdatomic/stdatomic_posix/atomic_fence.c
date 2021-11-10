b58e6e973df3e479b82de5c3642a0f6cc150b8b4
faultroll <boxer1992@qq.com>
2021-11-10 18:58:18 +0800

    [CHG] remove unneeded in subtree

--- rtc_base/c11runtime/stdatomic/stdatomic_posix/atomic_fence.c
+++
@@ -1,9 +0,0 @@
-#include "atomic_fence.h"
-
-void (atomic_thread_fence)(memory_order mo) {
-	atomic_thread_fence(mo);
-}
-
-void (atomic_signal_fence)(memory_order mo) {
-	atomic_signal_fence(mo);
-}
